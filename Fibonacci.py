b72c160bc085bb4f0d2016dc894a6472cf21c043
Guilherme Dannilo Maciel Coelho <gmacielcoelho@gmail.com>
2022-02-02 16:47:33 -0300

    Fibonacci-V1.0

---
+++ Fibonacci.py
@@ -0,0 +1,8 @@
+# ========== Sequencia de fibonacci ==========
+print("="*15,  end=' ')
+print("Sequencia de fibonacci ", end='')
+print("="*15)
+valorSequencial = int(input("Valor para a sequencia: "))
+
+
+
