96bd693c5006c1ed7e807cabec40e50e3265007b
Guilherme Dannilo Maciel Coelho <gmacielcoelho@gmail.com>
2022-02-02 16:57:33 -0300

    versão com for()

--- Fibonacci.py
+++ Fibonacci.py
@@ -3,6 +3,15 @@ print("="*15,  end=' ')
 print("Sequencia de fibonacci ", end='')
 print("="*15)
 valorSequencial = int(input("Valor para a sequencia: "))
+valorInicial = 0
+valorDois = 1
+valorFinal = 2
+print(f" {valorInicial} -> {valorDois}", end='')
+for i in range(valorSequencial):
+    print(f" -> {valorFinal} ", end='')
+    valorInicial = valorDois
+    valorDois = valorFinal
+    valorFinal = valorInicial + valorDois
 
 
 
